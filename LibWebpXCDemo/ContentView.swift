fb0a547946ebe93f1163e7c7cd304390f7224053
Lex Tang <lexrus@gmail.com>
2021-03-17 22:48:04 +0800

    initial commit

---
+++ LibWebpXCDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LibWebpXCDemo
+//
+//  Created by Lex on 3/17/21.
+//
+
+import SwiftUI
+import libwebp
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LibWebpXCDemo/LibWebpXCDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibWebpXCDemoApp.swift
+//  LibWebpXCDemo
+//
+//  Created by Lex on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LibWebpXCDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
